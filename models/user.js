6c5bedf9a1eb17e9fa85c6ce7cf1eed7c4cd4e18
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-29 00:36:35 +0530

    added user schema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			maxLength: 32,
+			trim: true,
+		},
+		user: {
+			type: String,
+			required: true,
+		},
+		bio: {
+			type: String,
+		},
+		photo: {
+			type: String,
+			required: true,
+		},
+		likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		dislikes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
