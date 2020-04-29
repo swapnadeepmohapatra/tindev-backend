e2f48540d00e3175b7f58d43a5119e5cf9c86f37
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-29 10:44:44 +0530

    connected mongodb and socket.io

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const connectedUsers = {};
+
+mongoose
+	.connect('mongodb://localhost:27017/tindev', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log('DB Connected');
+	})
+	.catch((error) => {
+		console.log(error);
+	});
+
+io.on('connection', (socket) => {
+	const { user } = socket.handshake.query;
+
+	connectedUsers[user] = socket.id;
+});
+
+app.use((req, res, next) => {
+	req.io = io;
+	req.connectedUsers = connectedUsers;
+
+	return next();
+});
+
+app.use(cors());
+
+app.use(express.json());
 
 app.use('/', (req, res) => {
 	res.json({ message: 'Welcome to Tindev' });
 });
 
 const port = process.env.PORT || 4242;
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Listening to port ${port}`);
 });
